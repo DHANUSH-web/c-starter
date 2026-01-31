cb0eb1ee54190354074c7c4618b204dabac8ff37
Dhanush H V <dhanushhv75@gmail.com>
2026-01-31 20:59:34 +0530

    Updated demo script to use logs folder for log files

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
-#include <stdio.h>
+#include <logger/logger.h>
 
 int main() {
+    struct LOGGER logger = INIT_LOGGER("main", "logs", "main.log", TRUE);
+
+    LOG(logger, "Starting logger...", INFO, TRUE);
     printf("Hello, World!\n");
+    LOG(logger, "Stopping logger...", INFO, TRUE);
+
+    EXIT_LOGGER(&logger);
     return 0;
 }
