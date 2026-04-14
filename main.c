79fe65b59112f5cc30c1b2470ad915bac657049f
Dhanush H V <dhanushhv75@gmail.com>
2026-04-15 00:55:42 +0530

    Rasync zip

--- main.c
+++ main.c
@@ -1,12 +1,268 @@
+#include <string.h>
+#include <stdio.h>
+#include <string.h>
 #include <logger/logger.h>
+#include <math.h>
 
-int main() {
-    struct LOGGER logger = INIT_LOGGER("main", "logs", "main.log", TRUE);
+#define MAX_BITS 64
 
-    LOG(logger, "Starting logger...", INFO, TRUE);
-    printf("Hello, World!\n");
-    LOG(logger, "Stopping logger...", INFO, TRUE);
+// BEGIN: MAIN UTILS
 
-    EXIT_LOGGER(&logger);
+int MAX(const int a, const int b)
+{
+    return a > b ? a : b;
+}
+
+double D_MAX(const double a, const double b)
+{
+    return a > b ? a : b;
+}
+
+float F_MAX(const float a, const float b)
+{
+    return a > b ? a : b;
+}
+
+int MIN(const int a, const int b)
+{
+    return a < b ? a : b;
+}
+
+double D_MIN(const double a, const double b)
+{
+    return a < b ? a : b;
+}
+
+float F_MIN(const float a, const float b)
+{
+    return a < b ? a : b;
+}
+
+int ABS(const int a)
+{
+    return a < 0 ? -a : a;
+}
+
+double D_ABS(const double a)
+{
+    return a < 0 ? -a : a;
+}
+
+float F_ABS(const float a)
+{
+    return a < 0 ? -a : a;
+}
+
+// ============ BLACK HOLE ============
+
+// Universal CONSTANTS in SI units
+const double G = 6.67430e-11;           // Gravitational constant (m^3 kg^-1 s^-2)
+const long int C = 299792458;           // Speed of light (m/s)
+const double SOLAR_MASS = 1.98847e30;   // Mass of our sun (kg)
+
+// #define G 6.67430e-11           // Gravitational constant (m^3 kg^-1 s^-2)
+// #define C 299792458.0           // Speed of light (m/s)
+// #define SOLAR_MASS 1.98847e30   // Mass of our sun (kg)
+
+// convert mass from kg to solar mass
+const double to_solar_mass(const double mass_kg)
+{
+    return mass_kg / SOLAR_MASS;
+}
+
+// convert mass from solar mass to kg
+const double to_kg(const double solar_masses)
+{
+    return solar_masses * SOLAR_MASS;
+}
+
+/*
+    Logic1: Mass from Orbital Data (Keplerian)
+    This is the "Measurement" method. You can calculate the mass if you know how fast a star
+    is moving (v) at a certain distance (r)
+
+    v = velocity in ms
+    r = radius in m
+
+    M = (v^2 * r) / G
+*/
+const double mass_from_orbit(const double r, const double v)
+{
+    const double mass_kg = ((v * v) * r) / G;
+    return mass_kg;
+}
+
+/*
+    Logic2: Radius from MASS (Schwarzschild)
+    If you already have the mass in solar masses, you can calculate the radius of event horizon
+    and you can calculate how much big the black hole actually is
+
+    Rs = (2 * G * M) / C^2
+    Rs = schwarzschild radius
+    G = gravitational constant
+    M = Mass of black hole in kg
+    C = speed of light
+*/
+const double get_schwarzschild_radius(const double mass_kg)
+{
+    const double sr = (2 * G * mass_kg) / (double)(C * C);
+    return sr;
+}
+
+/*
+    Logic3: Time dilation (interstellar effect)
+    This method can be used to calculate how much the time got slowed near a certain distance
+
+    t0 = tf * { 1 - (Rs / r) }
+*/
+const double calculate_time_dilation(const double mass_kg, const double distance_r, const double time_seconds)
+{
+    const double sr = get_schwarzschild_radius(mass_kg);
+
+    if (distance_r <= sr)
+    {
+        return -1.0;
+    }
+
+    const double time_dilation = time_seconds * sqrt( 1 - ( sr / distance_r ) );
+    return time_dilation;
+}
+
+// ============ BLACK HOLE ============
+
+// END: MAIN_UTILS
+
+int tea_party(const int tea, const int candy)
+{
+    return (tea < 5 || candy < 5) ? 0 : (tea >= candy * 2 || candy >= tea * 2) ? 2 : 1;
+}
+
+const char *fizz_string(const char *str)
+{
+    const size_t size = strlen(str);
+
+    if (str[0] == 'f' && str[size - 1] == 'b')
+        return "FizzBuzz";
+    if (str[0] == 'f')
+        return "Fizz";
+    if (str[size - 1] == 'b')
+        return "Buzz";
+
+    return str;
+}
+
+const char *fizz_string2(const int n)
+{
+    if (n % 3 == 0 && n % 5 == 0)
+        return "FizzBuzz!";
+    if (n % 3 == 0)
+        return "Fizz!";
+    if (n % 5 == 0)
+        return "Buzz!";
+
+    char *buffer = malloc(20);
+    sprintf(buffer, "%d", n);
+    strcat(buffer, "!");
+
+    return buffer;
+}
+
+BOOL two_as_one(const int a, const int b, const int c)
+{
+    return a + b == c || a + c == b || b + c == a;
+}
+
+BOOL in_order(
+    const int a,
+    const int b,
+    const int c,
+    BOOL bOk)
+{
+    return (bOk || b > a) && c > b;
+}
+
+BOOL in_order_equal(
+    const int a,
+    const int b,
+    const int c,
+    const BOOL equal_ok)
+{
+    return (equal_ok && a <= b && b <= c) || (a < b && b < c);
+}
+
+BOOL last_digit(const int a, const int b, const int c)
+{
+    return a % 10 == b % 10 || b % 10 == c % 10 || a % 10 == c % 10;
+}
+
+BOOL less_by_10(const int a, const int b, const int c)
+{
+    return abs(a - b) >= 10 || abs(b - c) >= 10 || abs(a - c) >= 10;
+}
+
+int without_doubles(const int die1, const int die2, const BOOL no_doubles)
+{
+    return no_doubles && die1 == die2 ? (die1 == 6 ? 1 : die1 + 1) + die2 : die1 + die2;
+}
+
+int max_mod5(const int a, const int b)
+{
+    return a == b ? 0 : a % 5 == b % 5 ? MIN(a, b) : MAX(a, b);
+}
+
+int red_ticket(const int a, const int b, const int c)
+{
+    return a == 2 && b == 2 && c == 2 ? 10 : a == b && b == c ? 5 : a != b && a != c ? 1 : 0;
+}
+
+int green_ticket(const int a, const int b, const int c)
+{
+    return a == b && b == c ? 20 : a == b || a == c || b == c ? 10 : 0;
+}
+
+char* to_binary(unsigned int n)
+{
+    char* bits = malloc(MAX_BITS + 1);
+
+    if (!bits) return NULL;
+
+    bits[MAX_BITS] = '\0';
+    int i = MAX_BITS - 1;
+
+    do
+    {
+        bits[i--] = (char)((n & 1) + '0');
+        n >>= 1;
+    } while (n);
+
+    memmove(bits, bits + i + 1, MAX_BITS - i + 2);
+    return bits;
+}
+
+#ifndef UNIT_TEST
+int main()
+{
+    // printf("Run `./build test` to test your logic!!\n");
+
+    // BLACK HOLE SIMULATION
+
+    // printf("Black Hole Simulation\n");
+
+    // const char* BH = "Sagittarius A*";
+    // const double mass_kg = mass_from_orbit(1e9, 2e7);
+    // const double solar_masses = to_solar_mass(mass_kg);
+    // const double sr = get_schwarzschild_radius(mass_kg);
+
+    // printf("Black Hole: %s\n", BH);
+    // printf("Mass of Black Hole: %f KG\n", mass_kg);
+    // printf("Mass of Black Hole: %f SM\n", solar_masses);
+    // printf("Schwarzschild Radius: %f M\n", sr);
+    // BLACK HOLE SIMULATION
+
+    char* bits = to_binary(27);
+    printf("0b'%s\n", bits);
+    free(bits);
+
     return 0;
 }
+#endif
---
+++ tests/test_main.c
@@ -0,0 +1,205 @@
+#include <unity.h>
+#include <logger.h>
+#include <main.h>
+#include <string.h>
+
+void setUp(void) {}
+
+// ********************* UTILS *********************
+void test_MAX(void)
+{
+    TEST_ASSERT_EQUAL(1, MAX(0, 1));
+    TEST_ASSERT_EQUAL(5, MAX(1, 5));
+    TEST_ASSERT_EQUAL(121, MAX(121, -121));
+}
+
+void test_D_MAX(void)
+{
+    TEST_ASSERT_EQUAL(1.5, D_MAX(0.2, 1.5));
+    TEST_ASSERT_EQUAL(5.6, D_MAX(1.7, 5.6));
+    TEST_ASSERT_EQUAL(121.1, D_MAX(121.1, -121.1));
+}
+
+void test_F_MAX(void)
+{
+    TEST_ASSERT_EQUAL(1.5f, F_MAX(0.2f, 1.5f));
+    TEST_ASSERT_EQUAL(5.6f, F_MAX(1.7f, 5.6f));
+    TEST_ASSERT_EQUAL(121.1f, F_MAX(121.1f, -121.1f));
+}
+
+void test_MIN(void)
+{
+    TEST_ASSERT_EQUAL(-1, MIN(-1, 1));
+    TEST_ASSERT_EQUAL(2, MIN(26, 2));
+    TEST_ASSERT_EQUAL(-29, MIN(0, -29));
+}
+
+void test_D_MIN(void)
+{
+    TEST_ASSERT_EQUAL(-1.6, D_MIN(-1.6, 1.0));
+    TEST_ASSERT_EQUAL(26.0, D_MIN(26.0, 26.1));
+    TEST_ASSERT_EQUAL(-29.0, D_MIN(29.0, -29.0));
+}
+
+void test_F_MIN(void)
+{
+    TEST_ASSERT_EQUAL(-1.6f, F_MIN(-1.6f, 1.0f));
+    TEST_ASSERT_EQUAL(26.0f, F_MIN(26.0f, 26.1f));
+    TEST_ASSERT_EQUAL(-29.0f, F_MIN(29.0f, -29.0f));
+}
+
+void test_ABS(void)
+{
+    TEST_ASSERT_EQUAL(1, ABS(1));
+    TEST_ASSERT_EQUAL(1, ABS(-1));
+    TEST_ASSERT_EQUAL(0, ABS(0));
+}
+
+void test_D_ABS(void)
+{
+    TEST_ASSERT_EQUAL(1.2, D_ABS(1.2));
+    TEST_ASSERT_EQUAL(1.6, D_ABS(-1.6));
+    TEST_ASSERT_EQUAL(0.0, D_ABS(0.0));
+}
+
+void test_F_ABS(void)
+{
+    TEST_ASSERT_EQUAL(1.2f, F_ABS(1.2f));
+    TEST_ASSERT_EQUAL(1.6f, F_ABS(-1.6f));
+    TEST_ASSERT_EQUAL(0.0f, F_ABS(0.0f));
+}
+// ********************* UTILS *********************
+
+// ********************* MAIN *********************
+void test_tea_party(void)
+{
+    TEST_ASSERT_EQUAL(1, tea_party(6, 8));
+    TEST_ASSERT_EQUAL(0, tea_party(3, 8));
+    TEST_ASSERT_EQUAL(2, tea_party(20, 6));
+}
+
+void test_fizz_string(void)
+{
+    TEST_ASSERT_EQUAL("Fizz", fizz_string("fig"));
+    TEST_ASSERT_EQUAL("Buzz", fizz_string("dib"));
+    TEST_ASSERT_EQUAL("FizzBuzz", fizz_string("fib"));
+    TEST_ASSERT_EQUAL("hello", fizz_string("hello"));
+}
+
+void test_fizz_string2(void)
+{
+    TEST_ASSERT_FALSE(strcmp("1!", fizz_string2(1)));
+    TEST_ASSERT_FALSE(strcmp("2!", fizz_string2(2)));
+    TEST_ASSERT_FALSE(strcmp("Fizz!", fizz_string2(3)));
+}
+
+void test_two_as_one(void)
+{
+    TEST_ASSERT_TRUE(two_as_one(1, 2, 3));
+    TEST_ASSERT_TRUE(two_as_one(3, 1, 2));
+    TEST_ASSERT_FALSE(two_as_one(3, 2, 2));
+}
+
+void test_in_order(void)
+{
+    TEST_ASSERT_TRUE(in_order(1, 2, 4, FALSE));
+    TEST_ASSERT_FALSE(in_order(1, 2, 1, FALSE));
+    TEST_ASSERT_TRUE(in_order(1, 1, 2, TRUE));
+}
+
+void test_in_order_equal(void)
+{
+    TEST_ASSERT_TRUE(in_order_equal(2, 5, 11, FALSE));
+    TEST_ASSERT_FALSE(in_order_equal(5, 7, 6, FALSE));
+    TEST_ASSERT_TRUE(in_order_equal(5, 5, 7, TRUE));
+}
+
+void test_last_digit(void)
+{
+    TEST_ASSERT_TRUE(last_digit(25, 75, 30));
+    TEST_ASSERT_FALSE(last_digit(45, 23, 30));
+    TEST_ASSERT_TRUE(last_digit(25, 25, 30));
+}
+
+void test_less_by_10(void)
+{
+    TEST_ASSERT_TRUE(less_by_10(1, 2, 11));
+    TEST_ASSERT_FALSE(less_by_10(1, 2, 3));
+    TEST_ASSERT_TRUE(less_by_10(1, 12, 0));
+}
+
+void test_without_doubles(void)
+{
+    TEST_ASSERT_EQUAL(5, without_doubles(2, 3, TRUE));
+    TEST_ASSERT_EQUAL(7, without_doubles(3, 3, TRUE));
+    TEST_ASSERT_EQUAL(6, without_doubles(3, 3, FALSE));
+}
+
+void test_max_mod5(void)
+{
+    TEST_ASSERT_EQUAL(3, max_mod5(2, 3));
+    TEST_ASSERT_EQUAL(6, max_mod5(6, 2));
+    TEST_ASSERT_EQUAL(3, max_mod5(3, 2));
+}
+
+void test_red_ticket(void)
+{
+    TEST_ASSERT_EQUAL(10, red_ticket(2, 2, 2));
+    TEST_ASSERT_EQUAL(0, red_ticket(2, 2, 1));
+    TEST_ASSERT_EQUAL(5, red_ticket(0, 0, 0));
+}
+
+void test_green_ticket(void)
+{
+    TEST_ASSERT_EQUAL(0, green_ticket(1, 2, 3));
+    TEST_ASSERT_EQUAL(20, green_ticket(2, 2, 2));
+    TEST_ASSERT_EQUAL(10, green_ticket(1, 1, 2));
+}
+
+void test_to_binary(void)
+{
+    char* TEST1 = to_binary(27);
+    char* TEST2 = to_binary(1);
+    char* TEST3 = to_binary(32);
+
+    TEST_ASSERT_EQUAL_STRING("11011", TEST1);
+    TEST_ASSERT_EQUAL_STRING("1", TEST2);
+    TEST_ASSERT_EQUAL_STRING("100000", TEST3);
+
+    free(TEST1);
+    free(TEST2);
+    free(TEST3);
+}
+// ********************* MAIN *********************
+
+void tearDown(void) {}
+
+int main(void)
+{
+    UNITY_BEGIN();
+
+    RUN_TEST(test_MAX);
+    RUN_TEST(test_D_MAX);
+    RUN_TEST(test_F_MAX);
+    RUN_TEST(test_MIN);
+    RUN_TEST(test_D_MIN);
+    RUN_TEST(test_F_MIN);
+    RUN_TEST(test_ABS);
+    RUN_TEST(test_D_ABS);
+    RUN_TEST(test_F_ABS);
+    RUN_TEST(test_tea_party);
+    RUN_TEST(test_fizz_string);
+    RUN_TEST(test_fizz_string2);
+    RUN_TEST(test_two_as_one);
+    RUN_TEST(test_in_order);
+    RUN_TEST(test_in_order_equal);
+    RUN_TEST(test_last_digit);
+    RUN_TEST(test_less_by_10);
+    RUN_TEST(test_without_doubles);
+    RUN_TEST(test_max_mod5);
+    RUN_TEST(test_red_ticket);
+    RUN_TEST(test_green_ticket);
+    RUN_TEST(test_to_binary);
+
+    return UNITY_END();
+}
